d201d9dcc3baaf1e0f1ba2a550bb7c1fa58df27b
Ivelina <iv4etoxx@gmail.com>
2013-04-30 16:19:20 +0300

    Put some blank lines and remove some blank lines, where StyleCop suggests (LabyrinthMatrix.cs)

--- Labyrinth-4/LabyrinthMatrix.cs
+++ Labyrinth-4/LabyrinthMatrix.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Labyrinth
 {
@@ -11,18 +9,18 @@ namespace Labyrinth
         private int myPostionVertical;
         private int myPostionHorizontal;
         private Random random = new Random();
+
         public LabyrinthMatrix()
         {
             myPostionHorizontal = 3;
             myPostionVertical = 3;
             matrix = new char[7][];
 
-
-
-            for (int i = 0; i <matrix.Length; i++)
+            for (int i = 0; i < matrix.Length; i++)
             {
                 matrix[i] = new char[7];
             }
+
             for (int i = 0; i < matrix.Length; i++)
             {
                 for (int j = 0; j < matrix[i].Length; j++)
@@ -33,56 +31,56 @@ namespace Labyrinth
 
             matrix[3][3] = '-';
         }
+
         public LabyrinthMatrix(LabyrinthMatrix l)
         {
+        }
 
+        public char[][] Matrix
+        {
+            get
+            {
+                return matrix;
+            }
+        }
+
+        public int MyPostionHorizontal
+        {
+            get
+            {
+                return this.myPostionHorizontal;
+            }
+
+            set
+            {
+                this.myPostionHorizontal = value;
+            }
+        }
+
+        public int MyPostionVertical
+        {
+            get
+            {
+                return this.myPostionVertical;
+            }
+
+            set
+            {
+                this.myPostionVertical = value;
+            }
         }
 
         private char GetRandomSymbol() 
         {
-           
-            int random1 = random.Next(0,2);
+            int random1 = random.Next(0, 2);
             if (random1 == 1)
             {
-
-
                 return 'X';
             }
             else
             {
                 return '-';
             }
-        }
-        public char[][] Matrix 
-        {
-            get 
-            {
-                return matrix;
-            }
-        }
-        public int MyPostionVertical 
-        {
-            get 
-            {
-                return this.myPostionVertical;
-            }
-            set 
-
-
-            {
-                this.myPostionVertical = value;
-            }
-        }
-        public int MyPostionHorizontal 
-        {
-            get 
-            {
-                return this.myPostionHorizontal;
-            }
-            set 
-            {
-                this.myPostionHorizontal = value;
-            }
-        }
+        } 
     }
 }
