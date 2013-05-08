b1e50c886ead4c391e35c549c0a4abd75892fd72
Ivelina <iv4etoxx@gmail.com>
2013-05-08 22:21:20 +0300

    Rename main class to LabyrinthEngine.cs

---
+++ Labyrinth-4/LabyrinthEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Labyrinth
+{
+	// Nothing is more admirable than the fortitude with which millionaires tolerate the disadvantages of their wealth.
+
+    class LabyrinthEngine
+    {
+
+        static void Main(string[] args)
+        {
+            LabyrinthProcesor processor = new LabyrinthProcesor();
+
+            while (true)
+            {
+                processor.ShowLabyrinth();
+                processor.ShowInputMessage();
+                string input;
+                input = Console.ReadLine();
+                processor.HandleInput(input);
+            }
+        }
+    }
+}
