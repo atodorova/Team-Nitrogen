8c1dfb2d00c16c95d057ac9b3b21611c93333061
atodorova <a.s.todorova@gmail.com>
2013-05-15 14:29:16 +0300

    Adding test metods for HandleInput() in LabyrinthEngineTest.cs

--- LabyrinthTests/LabyrinthEngineTest.cs
+++ LabyrinthTests/LabyrinthEngineTest.cs
@@ -7,8 +7,39 @@ namespace LabyrinthTests
     public class LabyrinthEngineTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HandleInput_IsValedCommandW()
         {
+            string input = "w";
+            Labyrinth.LabyrinthEngine engine = new Labyrinth.LabyrinthEngine();
+            engine.HandleInput(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HandleInput_IsValedCommandLR()
+        {
+            string input = "lr";
+            Labyrinth.LabyrinthEngine engine = new Labyrinth.LabyrinthEngine();
+            engine.HandleInput(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HandleInput_IsValedCommandStart()
+        {
+            string input = "start";
+            Labyrinth.LabyrinthEngine engine = new Labyrinth.LabyrinthEngine();
+            engine.HandleInput(input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void HandleInput_IsValedCommandAt()
+        {
+            string input = "@";
+            Labyrinth.LabyrinthEngine engine = new Labyrinth.LabyrinthEngine();
+            engine.HandleInput(input);
         }
     }
 }
