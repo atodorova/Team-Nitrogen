8b16a4d06c5cfc49f9c2777f4267c0d0e5d9e9ab
Ivelina <iv4etoxx@gmail.com>
2013-05-17 09:20:31 +0300

    Add unit tests  for the changed class Scoreboard.

---
+++ LabyrinthTests/ScoreboardTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Labyrinth;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LabyrinthTests
+{
+    [TestClass]
+    public class ScoreboardTest
+    {
+        [TestMethod]
+        public void Scoreboard_HandleScoreboardOneName()
+        {
+            Scoreboard sb = new Scoreboard();
+            string result = sb.HandleScoreboard(7, "Gosho");
+            string expectedResult = "1. Gosho --> 7 moves.\r\n";
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void Scoreboard_HandleScoreboardThreeoNames()
+        {
+            Scoreboard sb = new Scoreboard();
+            string result = sb.HandleScoreboard(7, "Gosho");
+            result = sb.HandleScoreboard(3, "Pesho");
+            result = sb.HandleScoreboard(5, "Ivan");
+            string expectedResult = "1. Pesho --> 3 moves.\r\n" +
+                "2. Ivan --> 5 moves.\r\n3. Gosho --> 7 moves.\r\n";
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void Scoreboard_HandleScoreboardSixoNames()
+        {
+            Scoreboard sb = new Scoreboard();
+            string result = sb.HandleScoreboard(7, "Gosho");
+            result = sb.HandleScoreboard(3, "Pesho");
+            result = sb.HandleScoreboard(5, "Ivan");
+            result = sb.HandleScoreboard(2, "Mimi");
+            result = sb.HandleScoreboard(6, "Rosen");
+            result = sb.HandleScoreboard(10, "Bobi");
+            string expectedResult = "Your not good enough for the scoreboard :)\r\n";
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestMethod]
+        public void Scoreboard_ToStringEmptyScoreboard()
+        {
+            Scoreboard sb = new Scoreboard();
+            string result = sb.ToString();
+            string expectedResult = "The scoreboard is empty.\r\n";
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
